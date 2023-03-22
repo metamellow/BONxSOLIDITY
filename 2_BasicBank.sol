df1b17615e4184ee1063f008e289f0adb817b70a
s0crates <115264178+s0crates-eth@users.noreply.github.com>
2023-03-22 14:54:31 +0700

    Update 2_BasicBank.sol

--- 2_BasicBank.sol
+++ 2_BasicBank.sol
@@ -2,13 +2,18 @@
 
 pragma solidity ^0.8.9;
 
-contract BasicBank{
-mapping(address => uint256) public balances;
+contract BasicETHBank{
+    mapping(address => uint256) public bankBalances;
 
-function depositMoney()
-function withdraw Money()
+    function depositETH() external payable{
+        bankBalances[msg.sender] += msg.value;
+    }
 
+    function withdrawETH(uint _amount) public{
+        uint userBalance = bankBalances[msg.sender];
+        require(userBalance >= _amount, "Error1 - Withdraw canceled");
+        bankBalances[msg.sender] = userBalance - _amount;
+        (bool success, ) = msg.sender.call{value: userBalance}("");
+        require(success, "Error2 - Withdraw canceled");
+    }
 }
-
-// https://www.youtube.com/watch?v=5BYg79J4fC0
-
