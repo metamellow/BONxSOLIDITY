8f35af2859311a88b895272f08a4cec2c43bab9d
s0crates <115264178+s0crates-eth@users.noreply.github.com>
2023-03-22 15:14:23 +0700

    Update 2_BasicBank.sol

--- 2_BasicBank.sol
+++ 2_BasicBank.sol
@@ -1,19 +1,17 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.9;
 
 contract BasicETHBank{
-    mapping(address => uint256) public bankBalances;
+/*1*/ mapping(address => uint) public bankBalances;
 
-    function depositETH() external payable{
+/*2*/ function depositETH() external payable{
         bankBalances[msg.sender] += msg.value;
     }
-
-    function withdrawETH(uint _amount) public{
+/*3*/ function withdrawETH(uint _amount) public{
         uint userBalance = bankBalances[msg.sender];
         require(userBalance >= _amount, "Error1 - Withdraw canceled");
         bankBalances[msg.sender] = userBalance - _amount;
-        (bool success, ) = msg.sender.call{value: userBalance}("");
+        (bool success, ) = msg.sender.call{value: _amount}("");
         require(success, "Error2 - Withdraw canceled");
     }
 }
