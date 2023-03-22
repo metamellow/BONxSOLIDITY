dbfba3d6929af10bd9b990efd16cadd71b5eeeb6
s0crates <115264178+s0crates-eth@users.noreply.github.com>
2023-03-22 15:54:17 +0700

    Update 2_BasicBank.sol

--- 2_BasicBank.sol
+++ 2_BasicBank.sol
@@ -1,22 +1,33 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
-contract BasicETHBank{
-/* =============== BASIC ETH BANK ===============
-[1] Create a place to store bank users balances
-[2] Function that updates users balance to msg.value
---- 'payable' means it can receive 'value' (ETH)
-[3] 
-============================================== */
-/*1*/ mapping(address => uint) public bankBalances;
+    
+    // SPDX-License-Identifier: MIT
+    pragma solidity ^0.8.9;
 
-/*2*/ function depositETH() external payable{
-        bankBalances[msg.sender] += msg.value;
+    /* ================ BASIC ETH BANK ================
+    [1] Create a place to store bank users balances
+    [2] Function that updates users balance to msg.value
+    --- 'payable' means it can receive the 'value' (ETH)
+    [3] Function that starts to withdraw the '_amount'
+    --- a. record the current 'userBalance'
+    --- b. check userBalance is greater than '_amount'
+    --- c. update bankBalance to show remaining balance
+    --- d. send user _amount & show 'success' if finished
+    ================================================= */
+
+    contract BasicETHBank{
+        //[1]
+        mapping(address => uint) public bankBalances;
+
+        //[2]
+        function depositETH() external payable{
+            bankBalances[msg.sender] += msg.value;
+        }
+        
+        //[3]
+        function withdrawETH(uint _amount) public{
+            uint userBalance = bankBalances[msg.sender];
+            require(userBalance >= _amount, "Error1 - Withdraw canceled");
+            bankBalances[msg.sender] = userBalance - _amount;
+            (bool success, ) = msg.sender.call{value: _amount}("");
+            require(success, "Error2 - Withdraw canceled");
+        }
     }
-/*3*/ function withdrawETH(uint _amount) public{
-        uint userBalance = bankBalances[msg.sender];
-        require(userBalance >= _amount, "Error1 - Withdraw canceled");
-        bankBalances[msg.sender] = userBalance - _amount;
-        (bool success, ) = msg.sender.call{value: _amount}("");
-        require(success, "Error2 - Withdraw canceled");
-    }
-}
