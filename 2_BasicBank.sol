3507192d93b1a7f04e2d5c72fbbbea5c56dbd389
s0crates <115264178+s0crates-eth@users.noreply.github.com>
2023-03-22 14:09:27 +0700

    Update 2_BasicBank.sol

--- 2_BasicBank.sol
+++ 2_BasicBank.sol
@@ -1,1 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+contract BasicBank{
+mapping(address => uint256) public balances;
+
+function depositMoney()
+function withdraw Money()
+
+}
+
+// https://www.youtube.com/watch?v=5BYg79J4fC0
 
